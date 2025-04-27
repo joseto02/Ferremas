95fea3f0ab9e1b08b1c4b51deb0fed502d9c9db7
JoseDev <jom.gonzalez@duocuc.cl>
2025-04-26 21:16:20 -0400

    crear y editar ok, falta la bbdd

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from . import views
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", views.home, name="home"),
+    path("productos", views.productos, name="productos"),
+    path("productos/crear", views.crear_producto, name="crear_producto"),
+    path("productos/editar", views.editar_producto, name="editar_producto"),
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'app/home.html')
+
+def productos(request):
+    return render(request, 'productos/index.html')
+
+def crear_producto(request):
+    return render(request, "productos/crear.html")
+
+def editar_producto(request):
+    return render(request, "productos/editar.html")
